a8b306ef34fade656bf713bff91ae248fef41486
mavillar <marcos.villar@mercadolibre.com>
2023-04-30 18:00:13 -0300

    feature/base-of-proyect

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  modulePathIgnorePatterns: ['/lib'],
+  coverageDirectory: '<rootDir>/coverage',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts?(x)'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  coverageReporters: ['lcov', 'html', 'json'],
+  coveragePathIgnorePatterns: [
+    '.*/__tests__/.*',
+    '.*/mocks/.*',
+    '.*/config/.*',
+    '.*/build/.*',
+    '.*/.storybook/.*',
+    '.stories.',
+  ],
+};
