5bf7ac2b3f503b945490669e9fe0c508859f6048
mavillar <marcos.villar@mercadolibre.com>
2023-04-30 18:13:35 -0300

    [FEATURE][IMAGE]: Image component added with yours tests and snapshot

---
+++ src/Image/__test__/index.test.tsx
@@ -0,0 +1,33 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { create } from 'react-test-renderer';
+import Image from '..';
+
+describe('Image component', () => {
+  const props = {
+    src: 'https://example.com/image.jpg',
+    alt: 'Test image',
+    width: 100,
+    height: 100,
+    className: 'test-class',
+  };
+
+  test('snapshot of component', () => {
+    const component = create(<Image {...props} />);
+    const tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('renders an image with provided props', () => {
+    render(<Image {...props} />);
+
+    const imageElement = screen.getByAltText(props.alt);
+
+    expect(imageElement).toBeInTheDocument();
+    expect(imageElement).toHaveAttribute('src', props.src);
+    expect(imageElement).toHaveAttribute('width', props.width.toString());
+    expect(imageElement).toHaveAttribute('height', props.height.toString());
+    expect(imageElement).toHaveClass(props.className);
+  });
+});
